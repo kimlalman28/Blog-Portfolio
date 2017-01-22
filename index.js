bc19962ace60938f629177fe608ac9c80baad284
Kimberly Lalmansingh <kimlalman28@gmail.com>
2017-01-22 18:50:07 -0500

    Minor adjustments to code

--- index.js
+++ index.js
@@ -7,40 +7,37 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(express.static('public'));
+app.use(express.static('public')); //allow use to style.css
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //to use ejs
 app.set('blog', './views');
 app.set('portfolio', './views');
 
-var dbURL = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
+var dbURL = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog'; //connection string to database
 
-app.get('/blog', function (req, res) {
+app.get('/blog', function (req, res) { //get request for blog page
 	pg.connect(dbURL, function(err, client, done){
 		client.query('select * from posts', function(err, result){
-			res.render('blog', {data: result.rows})
+			res.render('blog', {data: result.rows}) 
 			done();
 			pg.end();
 		})
 	})
 })
 
-app.post('/blog', function(req, res){
+app.post('/blog', function(req, res){ //post request when user submits a post
 	pg.connect(dbURL, function(err, client, done){
 		client.query(`insert into posts(title, body) values ('${req.body.title}', '${req.body.body}')`,function(err, result){
-			res.redirect('blog');
+			res.redirect('/blog');
 			done();
 			pg.end();
 		})
 	})
 })
 
-app.get('/portfolio', function(req, res){
+app.get('/portfolio', function(req, res){ //get request for portfolio page. Only need to render when on page
 	res.render('portfolio')
 })
-
-
-
 
 
 app.listen(3000, function(){
